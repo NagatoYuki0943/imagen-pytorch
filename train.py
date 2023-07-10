b31169cc98f8d324edef9baa0307b09327bc61a6
Yuki <workcomputer.com>
2023-07-10 17:22:50 +0800

    train model successfully

--- train.py
+++ train.py
@@ -1,16 +1,17 @@
-from imagen_pytorch import Unet, SRUnet256, Imagen, ImagenTrainer
+from imagen_pytorch import Unet, BaseUnet64, SRUnet256, SRUnet1024, Imagen, ImagenTrainer
 from imagen_pytorch.data import Dataset
 from tqdm import tqdm
 
 
 if __name__ == "__main__":
     image_folder = r"../datasets/flowers"
-    results_folder = r"results/flowers/200000",
-    train_num_steps = 200000
+    results_folder = r"results/flowers/200000"
+    train_num_steps = 100000
+    max_batch_size = 16
 
     # unets for unconditional imagen
-    unet1 = Unet(
-        dim = 32,
+    unet1 = BaseUnet64(
+        dim = 64,
         dim_mults = (1, 2, 4, 8),
         num_resnet_blocks = 1,
         layer_attns = (False, False, False, True),
@@ -18,12 +19,20 @@ if __name__ == "__main__":
     )
 
     unet2 = SRUnet256(
-        dim = 32,
+        dim = 64,
         dim_mults = (1, 2, 4),
         num_resnet_blocks = (2, 4, 8),
         layer_attns = (False, False, True),
         layer_cross_attns = False
     )
+
+    # unet3 = SRUnet1024(
+    #     dim = 64,
+    #     dim_mults = (1, 2, 4),
+    #     num_resnet_blocks = (2, 4, 8),
+    #     layer_attns = (False, False, True),
+    #     layer_cross_attns = False
+    # )
 
     # imagen, which contains the unet above
     imagen = Imagen(
@@ -37,6 +46,7 @@ if __name__ == "__main__":
         imagen = imagen,
         split_valid_from_train = True,  # whether to split the validation dataset from the training
         checkpoint_path = results_folder,
+        checkpoint_every = 1000,
         use_lion = True,
         lr = 0.0001,
         eps = 1e-8,
@@ -47,7 +57,7 @@ if __name__ == "__main__":
     # instantiate your dataloader, which returns the necessary inputs to the DDPM as tuple in the order of images, text embeddings, then text masks. in this case, only images is returned as it is unconditional training
     dataset = Dataset(
         folder = image_folder,
-        image_size = 256
+        image_size = 512
     )
 
     trainer.add_train_dataset(dataset, batch_size = 16)
@@ -59,26 +69,28 @@ if __name__ == "__main__":
     from torch.utils.tensorboard import SummaryWriter
     tb_writer = SummaryWriter(log_dir=results_folder)
 
+    # Train an unet at a time
+    # https://github.com/lucidrains/imagen-pytorch/issues/142
+    # you cannot only train on one unet at a time. you will need to save the trainer into a checkpoint, and resume training on a new unet
+    unet_number = 1
+
     with tqdm(initial = 0, total = train_num_steps, disable = not trainer.is_main) as pbar:
         for i in range(train_num_steps):
-            loss = trainer.train_step(unet_number = 1, max_batch_size = 4)
+            loss = trainer.train_step(unet_number = unet_number, max_batch_size = max_batch_size)
+            trainer.update(unet_number=unet_number)
+
             pbar.set_description(f'loss: {loss:.4f}')
-            # tensorboard log
             tb_writer.add_scalar("loss", loss, i)
 
-            if not (i % 100):
-                valid_loss = trainer.valid_step(unet_number = 1, max_batch_size = 4)
-                trainer.print(f'valid loss: {valid_loss}')
-                # tensorboard log
+            if (i != 0) and (not (i % 1000)):
+                valid_loss = trainer.valid_step(unet_number = unet_number, max_batch_size = max_batch_size)
+                trainer.print(f'valid loss1: {valid_loss}, valid loss2: {valid_loss}')
                 tb_writer.add_scalar("valid loss", valid_loss, i)
 
-            if not (i % 1000) and trainer.is_main: # is_main makes sure this can run in distributed
+            if  (i != 0) and (not (i % 1000)) and trainer.is_main: # is_main makes sure this can run in distributed
                 # gen image
                 images = trainer.sample(batch_size = 1, return_pil_images = True) # returns List[Image]
                 images[0].save(f'{results_folder}/sample-{i // 100}.png')
-
-                # save model
-                trainer.save_to_checkpoint_folder()
 
             pbar.update(1)
 
